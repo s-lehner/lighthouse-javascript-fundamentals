020a20f1834f7458bc65ddf15f23674036092484
siobhan-lehner <siobhanlehner@gmail.com>
2023-03-10 02:51:14 +0000

    hello file

---
+++ hello.js
@@ -0,0 +1,11 @@
+const sayHello = function (name) {
+  console.log('Hello' + name);
+}
+sayHello('Caliban');
+
+
+/*const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+*/
